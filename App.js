fbe7b7b82101e4e9891a24447964c62254e7e303
eric-araujo <eric.araujolima@hotmail.com>
2020-05-24 19:29:28 -0300

    Removido ScrollView e adicionado FlatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import { Text, View, TextInput, TouchableOpacity, ScrollView, Image, AsyncStorage } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TextInput, TouchableOpacity, ScrollView, Image, AsyncStorage, Alert, FlatList } from 'react-native';
 
 import imageCard from './img/imageCard.png';
 
@@ -7,12 +7,39 @@ import styles from './css/style';
 
 export default function App() {
 
-  const [peso, setPeso] = useState('0');
-  const [altura, setAltura] = useState('0');
+  const [peso, setPeso] = useState(0);
+  const [altura, setAltura] = useState(0);
 
-  function calcularIMC() {
+  const objUsuarios = [
+    {
+      nome: 'Eric Araújo Lima',
+      imc: '25.00',
+      mensagem: 'Sobrepeso',
 
+    },
+    {
+      nome: 'Eric Araújo Lima',
+      imc: '25.00',
+      mensagem: 'Sobrepeso',
 
+    },
+    {
+      nome: 'Eric Araújo Lima',
+      imc: '25.00',
+      mensagem: 'Sobrepeso',
+
+    },
+  ];
+
+  function Card({ nome, imc, mensagem }) {
+    return (
+      <View style={styles.cartao}>
+        <Text style={styles.labelCard}>{nome}</Text>
+        <Text style={styles.labelCard}>IMC: {imc}}</Text>
+        <Text style={styles.labelCard}>{mensagem}</Text>
+        <Image style={styles.imageCard} source={imageCard}></Image>
+      </View>
+    );
   }
 
   return (
@@ -21,34 +48,18 @@ export default function App() {
       <View>
         <Text style={styles.labelFormulario}>Peso</Text>
         <TextInput name="peso" keyboardType="numeric" style={styles.input} onChangeText={(peso) => setPeso(peso)}></TextInput>
-  <Text style={styles.labelFormulario}>Altura {peso} {altura}</Text>
+        <Text style={styles.labelFormulario}>Altura</Text>
         <TextInput name="altura" keyboardType="numeric" style={styles.input} onChangeText={(altura) => setAltura(altura)}></TextInput>
       </View>
       <View style={styles.buttons}>
-        <TouchableOpacity style={styles.buttonCalcular} onPress={calcularIMC()}>
+        <TouchableOpacity style={styles.buttonCalcular}>
           <Text style={styles.labelCalcular}>Calcular</Text>
         </TouchableOpacity>
       </View>
-      <ScrollView>
-        <View style={styles.cartao}>
-          <Text style={styles.labelCard}>Eric Araújo Lima</Text>
-          <Text style={styles.labelCard}>IMC: 25.00</Text>
-          <Text style={styles.labelCard}>Sobrepeso</Text>
-          <Image style={styles.imageCard} source={imageCard}></Image>
-        </View>
-        <View style={styles.cartao}>
-          <Text style={styles.labelCard}>Eric Araújo Lima</Text>
-          <Text style={styles.labelCard}>IMC: 25.00</Text>
-          <Text style={styles.labelCard}>Sobrepeso</Text>
-          <Image style={styles.imageCard} source={imageCard}></Image>
-        </View>
-        <View style={styles.cartao}>
-          <Text style={styles.labelCard}>Eric Araújo Lima</Text>
-          <Text style={styles.labelCard}>IMC: 25.00</Text>
-          <Text style={styles.labelCard}>Sobrepeso</Text>
-          <Image style={styles.imageCard} source={imageCard}></Image>
-        </View>
-      </ScrollView>
+      <FlatList
+        data={objUsuarios}
+        renderItem={({ item }) => <Card nome={item.nome} imc={item.imc} mensagem={item.mensagem} />}
+      />
     </View>
   );
 }
