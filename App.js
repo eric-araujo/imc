42cb1ca76d412ef1f2cad729cd819267f96ced79
eric-araujo <eric.araujolima@hotmail.com>
2020-05-24 13:54:43 -0300

    Teclado númerico adicionado aos inputs

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, Image } from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, TextInput, TouchableOpacity, ScrollView, Image, AsyncStorage } from 'react-native';
 
 import imageCard from './img/imageCard.png';
 
 import styles from './css/style';
 
 export default function App() {
+
+  const [peso, setPeso] = useState('0');
+  const [altura, setAltura] = useState('0');
+
+  function calcularIMC() {
+
+
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Calculadora IMC</Text>
       <View>
         <Text style={styles.labelFormulario}>Peso</Text>
-        <TextInput style={styles.input}></TextInput>
-        <Text style={styles.labelFormulario}>Altura</Text>
-        <TextInput style={styles.input}></TextInput>
+        <TextInput name="peso" keyboardType="numeric" style={styles.input} onChangeText={(peso) => setPeso(peso)}></TextInput>
+  <Text style={styles.labelFormulario}>Altura {peso} {altura}</Text>
+        <TextInput name="altura" keyboardType="numeric" style={styles.input} onChangeText={(altura) => setAltura(altura)}></TextInput>
       </View>
       <View style={styles.buttons}>
-        <TouchableOpacity style={styles.buttonCalcular}>
+        <TouchableOpacity style={styles.buttonCalcular} onPress={calcularIMC()}>
           <Text style={styles.labelCalcular}>Calcular</Text>
         </TouchableOpacity>
       </View>
